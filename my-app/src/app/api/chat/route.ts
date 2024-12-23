5de6a8a8235b0a5b802763e607f0e67aff41ab2c
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-23 19:52:29 +0900

    add recoil and route.tsを追加

---
+++ my-app/src/app/api/chat/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+let chatMessages = [
+    {
+        id: 1,
+        role: "bot",
+        message: "こんにちは！",
+    },
+];
+
+export function GET() {
+    return NextResponse.json(chatMessages);
+}
+
+export async function POST(request: Request) {
+    const data = await request.json();
+
+    const newMessage = {
+        id: chatMessages.length + 1,
+        role: data.role || "user",
+        message: data.message,
+    };
+
+    chatMessages.push(newMessage);
+
+    chatMessages.push({
+        id: chatMessages.length + 1,
+        role: "bot",
+        message: data.message,
+    });
+
+    return NextResponse.json(newMessage);
+}
